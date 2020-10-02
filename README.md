5adab164cb6a7836c08ca3206a742bb8764e933b
Diego Estrada Talamantes <devdiegoest@gmail.com>
2020-10-01 18:42:03 -0600

    Initial empty shell

---
+++ README.md
@@ -0,0 +1,13 @@
+# NodeShare
+
+## Installation
+
+Run `npm install` and later `npm run client-install`
+
+After that add the .env file sent in the group to the root folder to set the
+environment variables
+
+## Running
+
+Run `npm run dev`, this will call npm start on both the back-end and the
+front-end
---
+++ server.js
@@ -0,0 +1,33 @@
+const { MongoClient } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(process.env.ATLAS_URI, {
+  useUnifiedTopology: true,
+});
+client.connect().then(() => {
+  console.log("MongoDB database connection established successfully");
+});
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
